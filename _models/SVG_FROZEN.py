420ca001126519424c3454670e1527ffb952ab37
Eli Wang <ewang921@gmail.com>
2021-02-03 17:34:05 -0800

    work on svg_frozen

--- _models/SVG_FROZEN.py
+++ _models/SVG_FROZEN.py
@@ -14,6 +14,65 @@ import torch.nn as nn
 import torch.optim as optim
 import tensorflow as tf
 
+TRAIN_EPOCHS = 5
+
+COLLIDE_CONFIG = {
+        'binary_labels': ['is_colliding_dynamic'],
+        'train_shift': [30, 1024, 1],
+        'train_len': 32470,
+        'test_shift': [30, 1024, 16],
+        'test_len': 62,
+        }
+
+TOWER_CONFIG = {
+        'binary_labels': ['is_stable'],
+        'train_shift': [0, 1024, 1],
+        'train_len': 63360,
+        'test_shift': [2, 1024, 32],
+        'test_len': 384,
+        }
+
+CONTAIN_CONFIG = {
+        'binary_labels': ['object_data'],
+        'train_shift': [30, 1024, 1],
+        'train_len': 36355,
+        'test_shift': [30, 1024, 16],
+        'test_len': 28,
+        }
+
+CLOTH_CONFIG = {
+        'binary_labels': ['object_category'],
+        'train_shift': [0, 1024, 1],
+        'train_len': 63360,
+        'test_shift': [2, 1024, 64],
+        'test_len': 192,
+        }
+
+ROLL_VS_SLIDE_CONFIG = {
+        'binary_labels': ['is_rolling'],
+        'train_shift': [32, 1024, 1],
+        'train_len': 74572 // 4,
+        'test_shift': [32, 1024, 64],
+        'test_len': 320 // 4,
+        }
+
+
+def get_config(subset):
+    if 'collide' in subset:
+        return COLLIDE_CONFIG
+    elif 'tower' in subset:
+        return TOWER_CONFIG
+    elif 'contain' in subset:
+        return CONTAIN_CONFIG
+    elif 'cloth' in subset:
+        return CLOTH_CONFIG
+    elif 'roll' in subset:
+        return ROLL_VS_SLIDE_CONFIG
+    elif 'slide' in subset:
+        return ROLL_VS_SLIDE_CONFIG
+    else:
+        raise ValueError("Unkown config for subset: %s" % subset)
+
 def filter_rule(data, keys):
     assert all(k in keys for k in ['is_moving', 'is_acting']), keys
     return tf.logical_and(data['is_moving'], tf.logical_not(data['is_acting']))
@@ -24,7 +83,7 @@ DATA_PARAMS = { # TODO: move to config?
     'sequence_len': 10,
     'buffer_size': 16,
     'batch_size': 1,
-    'shift_selector': slice(30, 1024, 1), # remove falling part of sequence
+    'shift_selector': slice(30, 1024, 1), # remove falling part of sequence TODO: have different settings for each dataset
     'test': False, # True, # determines if tf dataset doesn't loop
     'main_source_key': 'full_particles',
     'sources': ['images', 'reference_ids', 'object_data'],
@@ -43,27 +102,58 @@ def run(
     model_dir,
     write_feat='',
     ):
-    if write_feat:
-        test()
-    else:
-        train()
-
-def train():
+    print(name, datasets, model_dir)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
     device = torch.device('cpu') # TODO
-    encoder = 'deit' # TODO
-    # model = physion.modules.Frozen_MLP(encoder).to(device)
-    model = physion.modules.Frozen_LSTM(encoder).to(device)
+    config = {
+        'name': name,
+        'datapaths': datasets,
+        'encoder': 'deit',
+        'regressor': 'lstm',
+        'batch_size': 64,
+        'model_dir': model_dir,
+        'state_len': 4, # number of images as input
+        'device': device,
+    }
+    model =  get_model(config['regressor'], config['encoder']).to(device)
+    config['model'] = model
+    init_seed(seed)
+    if write_feat:
+        test(config)
+    else:
+        train(config)
+
+def init_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+def get_model(regressor, encoder):
+    if regressor == 'mlp':
+        return physion.modules.Frozen_MLP(encoder)
+    elif regressor == 'lstm':
+        return physion.modules.Frozen_LSTM(encoder)
+    else:
+        raise NotImplementedError
+
+def train(config):
+    device = config['device']
+    model = config['model']
+    model_file = os.path.join(config['model_dir'], 'model.pt')
+
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
 
     dataset = TDWDataset(
-        data_root=['/mnt/fs4/mrowca/neurips/images/rigid/collide2_new/new_tfdata'],
-        label_key='is_colliding_dynamic',
+        data_root=config['datapaths'],
+        label_key='object_data', # just use object_data here since it doesn't really matter
         DATA_PARAMS=DATA_PARAMS,
         )
-    trainloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    trainloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=True)
 
-    for epoch in range(5):
+    best_loss = 1e9
+    for epoch in range(TRAIN_EPOCHS):
         running_loss = 0.
         for i, data in enumerate(trainloader):
             images = data['images'].to(device)
@@ -77,23 +167,36 @@ def train():
             optimizer.step()
 
             running_loss += loss.item()
-            print(running_loss/(i+1))
+            avg_loss = running_loss/(i+1)
+            print(avg_loss)
 
-def test():
-    device = torch.device('cpu') # TODO
-    encoder = 'deit' # TODO
-    # model = physion.modules.Frozen_MLP(encoder).to(device)
-    model = physion.modules.Frozen_LSTM(encoder).to(device)
+        # save model
+        if avg_loss < best_loss:
+            best_loss = avg_loss
+            torch.save(model.state_dict(), model_file)
 
+def get_label_key(subsets):
+    if len(subsets) > 1:
+        raise NotImplementedError('label key is not used for training')
+    else:
+        return get_config(subsets[0])['binary_labels'][0]
+
+def test(config):
+    device = config['device']
+    encoder = config['encoder']
+    state_len = config['state_len']
+    model = config['model']
+
+    subsets = [subset.split('/')[-1] for subset in config['datapaths']]
     dataset = TDWDataset(
-        data_root=['/mnt/fs4/mrowca/neurips/images/rigid/collide2_new/new_tfvaldata'],
-        label_key='is_colliding_dynamic',
+        data_root=config['datapaths'],
+        label_key=get_label_key(subsets),
+        train=False,
         DATA_PARAMS=DATA_PARAMS,
         size=10, # TODO
         )
-    testloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    testloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=False)
 
-    state_len = 4
     extracted_feats = []
     for i, data in enumerate(testloader):
         images = data['images'].to(device)
@@ -105,7 +208,7 @@ def test():
 
         for step in range(rollout_steps):
             input_feats = rollout_states[-state_len:]
-            pred_state  = model.dynamics(input_feats) # TODO: implement rollout instead of using images
+            pred_state  = model.dynamics(input_feats) # dynamics model predicts next latent from past latents
             rollout_states.append(pred_state)
 
         extracted_feats.append({ # TODO: to cpu?
@@ -115,6 +218,12 @@ def test():
         })
 
     # Save out features
+    feat_path = os.path.join(config['model_dir'], 'features')
+    if not os.path.exists(feat_path):
+        os.makedirs(feat_path)
+    feat_fn = os.path.join(feat_path, config['name']+'.pkl')
+    pickle.dump(extracted_feats, open(feat_fn, 'wb')) 
+    print('Saved features to {}'.format(feat_fn))
 
 class Objective():
     def __init__(self,
