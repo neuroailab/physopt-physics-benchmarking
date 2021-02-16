6e81471ad3027574761fce44c803f1e80abdd872
Elias Wang <ewang921@gmail.com>
2021-02-16 06:13:18 -0800

    switch debug data from cloth to collide

--- physopt/data/debug_space.py
+++ physopt/data/debug_space.py
@@ -15,21 +15,20 @@ TRAIN = {k: os.path.join(TRAIN_BASE_DIR, k) for k in os.listdir(TRAIN_BASE_DIR)}
 HUMAN = {k: os.path.join(HUMAN_BASE_DIR, k) for k in os.listdir(HUMAN_BASE_DIR)}
 
 # Data subsets
-_CLOTH = {'name': 'cloth', 'data': [TRAIN['cloth_on_object']]}
-
-_HUMAN_CLOTH = {'name': 'human_cloth', 'data': [HUMAN['cloth_on_object']]}
+_COLLIDE = {'name': 'collide', 'data': [TRAIN['collide2_new']]}
+_HUMAN_COLLIDE_1 = {'name': 'human_collide_1', 'data': [HUMAN['collide2_new_1']]}
 
 # Spaces
 SEEDS = list(range(_NUM_SEEDS))
 
-TRAIN_DATA = get_all_subsets([_CLOTH,]) # TODO: use combined dataset?
+TRAIN_DATA = get_all_subsets([_COLLIDE,]) # TODO: use combined dataset?
 
-TRAIN_FEAT_DATA = [_CLOTH,]
+TRAIN_FEAT_DATA = [_COLLIDE,]
 
-TEST_FEAT_DATA = [_HUMAN_CLOTH,]
+TEST_FEAT_DATA = [_HUMAN_COLLIDE_1,]
 
 METRICS_DATA = [
-        (_CLOTH, _HUMAN_CLOTH),
+        (_COLLIDE, _HUMAN_COLLIDE_1),
         ]
 
 SPACE = construct_data_spaces(SEEDS, TRAIN_DATA, TRAIN_FEAT_DATA, TEST_FEAT_DATA, METRICS_DATA)
