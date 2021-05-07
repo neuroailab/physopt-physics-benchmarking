472ac2edf1be6884826fc54acf88b18c5e9ecefe
Damian Mrowca <mrowca@gmx.de>
2021-05-06 17:45:38 -0700

    physion domino space updated

--- physopt/data/physion_space.py
+++ physopt/data/physion_space.py
@@ -4,13 +4,28 @@ from physopt.data.data_space import get_all_subsets, construct_data_spaces
 
 _NUM_SEEDS = 1
 
-TRAIN_BASE_DIR = '/mnt/fs4/eliwang/'
+DOMINO_BASE_DIR = '/mnt/fs4/eliwang/dominoes'
+SUBSETS = [
+        'pilot_dominoes_0mid_d3chairs_o1plants_tdwroom',
+        'pilot_dominoes_1mid_J025R45_boxroom',
+        'pilot_dominoes_1mid_J025R45_o1flex_tdwroom',
+        'pilot_dominoes_2mid_J020R15_d3chairs_o1plants_tdwroom',
+        'pilot_dominoes_2mid_J025R30_tdwroom',
+        'pilot_dominoes_4mid_boxroom',
+        'pilot_dominoes_4midRM1_boxroom',
+        'pilot_dominoes_4midRM1_tdwroom',
+        'pilot_dominoes_4mid_tdwroom',
+        'pilot_dominoes_default_boxroom',
+        'pilot_dominoes_SJ020_d3chairs_o1plants_tdwroom',
+        ]
+
+
 
 # Data subsets
 _DOMINOES = {'name': 'dominoes',
-        'data': [os.path.join(TRAIN_BASE_DIR, 'new_dominoes')]}
+        'data': [os.path.join(DOMINO_BASE_DIR, s, 'train') for s in SUBSETS]}
 _TEST_DOMINOES = {'name': 'test_dominoes',
-        'data': ['/mnt/fs4/eliwang/dominoes/pilot_dominoes_1mid_J025R45_boxroom/test']}
+        'data': [os.path.join(DOMINO_BASE_DIR, s, 'test') for s in SUBSETS]}
 
 # Spaces
 SEEDS = list(range(_NUM_SEEDS))
