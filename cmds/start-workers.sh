59db8d75552ef8e5c80a7e4b9b8d690e7c121e88
Damian Mrowca <mrowca@gmx.de>
2021-05-20 03:05:56 +0000

    start workers

--- cmds/start-workers.sh
+++ cmds/start-workers.sh
@@ -2,5 +2,5 @@ dbname=$1
 num_workers=$2
 
 for i in `seq 0 $(($num_workers - 1))`; do
-	tmux new -d -s worker${i} "source ~/.virtualenvs/physopt/bin/activate && export CUDA_VISIBLE_DEVICES=${i} && /home/mrowca_stanford_edu/workspace/hyperopt/scripts/hyperopt-mongo-worker --mongo=localhost:25555/${dbname} --logfile=/mnt/fs1/mrowca/workers/${dbname}-$(hostname)-worker${i}-logfile.txt"
+	tmux new -d -s worker${i} "source ~/.virtualenvs/physopt/bin/activate && export CUDA_VISIBLE_DEVICES=${i} && ~/workspace/hyperopt/scripts/hyperopt-mongo-worker --mongo=localhost:25555/${dbname} --logfile=/mnt/fs1/workers/${dbname}-$(hostname)-worker${i}-logfile.txt"
 done
