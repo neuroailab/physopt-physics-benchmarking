8f5039d58a43c669008f262f3adf0627e4f5a8fc
Damian Mrowca <mrowca@gmx.de>
2021-05-20 23:17:13 +0000

    updated decode functions

--- utils/decode_rpin_log.py
+++ utils/decode_rpin_log.py
@@ -1,22 +1,34 @@
+import sys
 import csv
 import numpy as np
 
-log_file = '/mnt/fs4/mrowca/hyperexamples/11000/RPIN/dominoes/0/model/log-050714-1799694.txt'
 
-header = np.array([['steps', 'mean', 'p_1', 'p_2', 'o_1', 'o_2']])
 
-with open(log_file, 'r') as f:
-    reader = csv.reader(f, delimiter='|')
-    csv_data = [[r.strip() for r in row] for row in reader]
+def decode_rpin_log(log_file):
+    header = np.array([['steps', 'mean', 'p_1', 'p_2', 'o_1', 'o_2']])
 
-# Remove invalid rows
-data = []
-for row in csv_data:
-    try:
-        float(row[1])
-        data.append([float(row[0][-5:-1])] + [float(r) for r in row[1:]])
-    except:
-        continue
+    with open(log_file, 'r') as f:
+        reader = csv.reader(f, delimiter='|')
+        csv_data = [[r.strip() for r in row] for row in reader]
 
-data = np.array(data)
-header_data = np.concatenate([header, data], axis=0)
+    # Remove invalid rows
+    data = []
+    for row in csv_data:
+        try:
+            float(row[1])
+            data.append([float(row[0][-5:-1])] + [float(r) for r in row[1:]])
+        except:
+            continue
+
+    data = np.array(data)
+    header_data = np.concatenate([header, data], axis=0)
+
+    return header, data
+
+
+if __name__ == '__main__':
+    #log_file = '/mnt/fs1/mrowca/test_coll/RPIN/collision/0/model/log-052007-3debcb7.txt'
+    log_file = sys.argv[1]
+    header, data = decode_rpin_log(log_file)
+    print(header)
+    print(data)
--- utils/decode_svg_log.py
+++ utils/decode_svg_log.py
@@ -1,22 +1,32 @@
+import sys
 import csv
 import numpy as np
 
-log_file = '/mnt/fs1/mrowca/test11/SVG/cloth/0/model/logs/log.txt'
 
-header = np.array([['epoch', 'mse loss', 'kld loss', 'steps']])
+def decode_svg_log(log_file):
+    header = np.array([['epoch', 'mse loss', 'kld loss', 'steps']])
 
-with open(log_file, 'r') as f:
-    reader = csv.reader(f, delimiter=' ')
-    csv_data = [[r.strip() for r in row] for row in reader]
+    with open(log_file, 'r') as f:
+        reader = csv.reader(f, delimiter=' ')
+        csv_data = [[r.strip() for r in row] for row in reader]
 
-data = []
-for row in csv_data:
-    data.append([
-        float(row[0][1:-1]),
-        float(row[3]),
-        float(row[7]),
-        float(row[8][1:-1]),
-        ])
+    data = []
+    for row in csv_data:
+        data.append([
+            float(row[0][1:-1]),
+            float(row[3]),
+            float(row[7]),
+            float(row[8][1:-1]),
+            ])
 
-data = np.array(data)
-header_data = np.concatenate([header, data], axis=0)
+    data = np.array(data)
+    header_data = np.concatenate([header, data], axis=0)
+
+    return header, data
+
+if __name__ == '__main__':
+    #log_file = '/mnt/fs1/mrowca/test11/SVG/cloth/0/model/logs/log.txt'
+    log_file = sys.argv[1]
+    header, data = decode_svg_log(log_file)
+    print(header)
+    print(data)
