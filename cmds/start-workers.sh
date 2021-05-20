3debcb7295ed033dadd7664582273586fe2133d4
Damian Mrowca <mrowca@gmx.de>
2021-05-20 07:26:12 +0000

    longer worker time out

--- cmds/start-workers.sh
+++ cmds/start-workers.sh
@@ -2,6 +2,6 @@ dbname=$1
 num_workers=$2
 
 for i in `seq 0 $(($num_workers - 1))`; do
-	tmux new -d -s worker${i} "source ~/.virtualenvs/physopt/bin/activate && export CUDA_VISIBLE_DEVICES=${i} && ~/workspace/hyperopt/scripts/hyperopt-mongo-worker --mongo=localhost:25555/${dbname} --logfile=/mnt/fs1/workers/${dbname}-$(hostname)-worker${i}-logfile.txt"
+	tmux new -d -s worker${i} "source ~/.virtualenvs/physopt/bin/activate && export CUDA_VISIBLE_DEVICES=${i} && ~/workspace/hyperopt/scripts/hyperopt-mongo-worker --mongo=localhost:25555/${dbname} --poll-interval=5 --reserve-timeout=60000.0 --logfile=/mnt/fs1/workers/${dbname}-$(hostname)-worker${i}-logfile.txt"
 	#sleep 1.0
 done
