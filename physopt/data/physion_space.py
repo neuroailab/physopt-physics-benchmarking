c6443f212ad561e35b3dd123a2d99abfa9c78049
eli <eliaszwang@outlook.com>
2021-05-27 12:46:44 +0000

    add paths for new testing data

--- physopt/data/physion_space.py
+++ physopt/data/physion_space.py
@@ -5,6 +5,7 @@ from physopt.data.data_space import get_all_subsets, construct_data_spaces
 _NUM_SEEDS = 1
 
 BASE_DIR = '/mnt/fs4/hsiaoyut/tdw_physics/data/'
+NEW_BASE_DIR = '/mnt/fs1/tdw_datasets/'
 
 # Data subsets
 _TRAIN_CLOTH = {'name': 'cloth',
@@ -12,57 +13,64 @@ _TRAIN_CLOTH = {'name': 'cloth',
 _TRAIN_FEAT_CLOTH = {'name': 'train_cloth',
         'data': [os.path.join(BASE_DIR, 'clothSagging/*/train_readout/*.hdf5')]}
 _TEST_FEAT_CLOTH = {'name': 'test_cloth',
-        'data': [os.path.join(BASE_DIR, 'clothSagging/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'clothSagging/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*clothSagging*/*/*.hdf5')]}
 
 _TRAIN_COLLISION = {'name': 'collision',
         'data': [os.path.join(BASE_DIR, 'collision/*/train/*.hdf5')]}
 _TRAIN_FEAT_COLLISION = {'name': 'train_collision',
         'data': [os.path.join(BASE_DIR, 'collision/*/train_readout/*.hdf5')]}
 _TEST_FEAT_COLLISION = {'name': 'test_collision',
-        'data': [os.path.join(BASE_DIR, 'collision/*/valid_readout/*.hdf5')]}
-
+        # 'data': [os.path.join(BASE_DIR, 'collision/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*collision*/*/*.hdf5')]}
 
 _TRAIN_CONTAINMENT = {'name': 'containment',
         'data': [os.path.join(BASE_DIR, 'containment/*/train/*.hdf5')]}
 _TRAIN_FEAT_CONTAINMENT = {'name': 'train_containment',
         'data': [os.path.join(BASE_DIR, 'containment/*/train_readout/*.hdf5')]}
 _TEST_FEAT_CONTAINMENT = {'name': 'test_containment',
-        'data': [os.path.join(BASE_DIR, 'containment/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'containment/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*containment*/*/*.hdf5')]}
 
 _TRAIN_DOMINOES = {'name': 'dominoes',
         'data': [os.path.join(BASE_DIR, 'dominoes/*/train/*.hdf5')]}
 _TRAIN_FEAT_DOMINOES = {'name': 'train_dominoes',
         'data': [os.path.join(BASE_DIR, 'dominoes/*/train_readout/*.hdf5')]}
 _TEST_FEAT_DOMINOES = {'name': 'test_dominoes',
-        'data': [os.path.join(BASE_DIR, 'dominoes/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'dominoes/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*dominoes*/*/*.hdf5')]}
 
 _TRAIN_DROP = {'name': 'drop',
         'data': [os.path.join(BASE_DIR, 'drop/*/train/*.hdf5')]}
 _TRAIN_FEAT_DROP = {'name': 'train_drop',
         'data': [os.path.join(BASE_DIR, 'drop/*/train_readout/*.hdf5')]}
 _TEST_FEAT_DROP = {'name': 'test_drop',
-        'data': [os.path.join(BASE_DIR, 'drop/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'drop/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*drop*/*/*.hdf5')]}
 
 _TRAIN_LINKING = {'name': 'linking',
         'data': [os.path.join(BASE_DIR, 'linking/*/train/*.hdf5')]}
 _TRAIN_FEAT_LINKING = {'name': 'train_linking',
         'data': [os.path.join(BASE_DIR, 'linking/*/train_readout/*.hdf5')]}
 _TEST_FEAT_LINKING = {'name': 'test_linking',
-        'data': [os.path.join(BASE_DIR, 'linking/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'linking/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*linking*/*/*.hdf5')]}
 
 _TRAIN_ROLLSLIDE = {'name': 'rollslide',
         'data': [os.path.join(BASE_DIR, 'rollingSliding/*/train/*.hdf5')]}
 _TRAIN_FEAT_ROLLSLIDE = {'name': 'train_rollslide',
         'data': [os.path.join(BASE_DIR, 'rollingSliding/*/train_readout/*.hdf5')]}
 _TEST_FEAT_ROLLSLIDE = {'name': 'test_rollslide',
-        'data': [os.path.join(BASE_DIR, 'rollingSliding/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'rollingSliding/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*rollingSliding*/*/*.hdf5')]}
 
 _TRAIN_TOWERS = {'name': 'towers',
         'data': [os.path.join(BASE_DIR, 'towers/*/train/*.hdf5')]}
 _TRAIN_FEAT_TOWERS = {'name': 'train_towers',
         'data': [os.path.join(BASE_DIR, 'towers/*/train_readout/*.hdf5')]}
 _TEST_FEAT_TOWERS = {'name': 'test_towers',
-        'data': [os.path.join(BASE_DIR, 'towers/*/valid_readout/*.hdf5')]}
+        # 'data': [os.path.join(BASE_DIR, 'towers/*/valid_readout/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*towers*/*/*.hdf5')]}
 
 # Spaces
 SEEDS = list(range(_NUM_SEEDS))
