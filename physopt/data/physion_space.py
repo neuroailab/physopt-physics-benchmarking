ac669c594285349564abb21f06296a17ce87c9ed
eli <eliaszwang@outlook.com>
2021-06-02 20:21:37 +0000

    make test_feat path more selective to prevent reading ones in .../tfrecords/*

--- physopt/data/physion_space.py
+++ physopt/data/physion_space.py
@@ -13,56 +13,56 @@ _TRAIN_CLOTH = {'name': 'cloth',
 _TRAIN_FEAT_CLOTH = {'name': 'train_cloth',
         'data': [os.path.join(BASE_DIR, 'clothSagging/*/train_readout/*.hdf5')]}
 _TEST_FEAT_CLOTH = {'name': 'test_cloth',
-        'data': [os.path.join(NEW_BASE_DIR, '*clothSagging*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*clothSagging*/*redyellow/*.hdf5')]}
 
 _TRAIN_COLLISION = {'name': 'collision',
         'data': [os.path.join(BASE_DIR, 'collision/*/train/*.hdf5')]}
 _TRAIN_FEAT_COLLISION = {'name': 'train_collision',
         'data': [os.path.join(BASE_DIR, 'collision/*/train_readout/*.hdf5')]}
 _TEST_FEAT_COLLISION = {'name': 'test_collision',
-        'data': [os.path.join(NEW_BASE_DIR, '*collision*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*collision*/*redyellow/*.hdf5')]}
 
 _TRAIN_CONTAINMENT = {'name': 'containment',
         'data': [os.path.join(BASE_DIR, 'containment/*/train/*.hdf5')]}
 _TRAIN_FEAT_CONTAINMENT = {'name': 'train_containment',
         'data': [os.path.join(BASE_DIR, 'containment/*/train_readout/*.hdf5')]}
 _TEST_FEAT_CONTAINMENT = {'name': 'test_containment',
-        'data': [os.path.join(NEW_BASE_DIR, '*containment*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*containment*/*redyellow/*.hdf5')]}
 
 _TRAIN_DOMINOES = {'name': 'dominoes',
         'data': [os.path.join(BASE_DIR, 'dominoes/*/train/*.hdf5')]}
 _TRAIN_FEAT_DOMINOES = {'name': 'train_dominoes',
         'data': [os.path.join(BASE_DIR, 'dominoes/*/train_readout/*.hdf5')]}
 _TEST_FEAT_DOMINOES = {'name': 'test_dominoes',
-        'data': [os.path.join(NEW_BASE_DIR, '*dominoes*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*dominoes*/*redyellow/*.hdf5')]}
 
 _TRAIN_DROP = {'name': 'drop',
         'data': [os.path.join(BASE_DIR, 'drop/*/train/*.hdf5')]}
 _TRAIN_FEAT_DROP = {'name': 'train_drop',
         'data': [os.path.join(BASE_DIR, 'drop/*/train_readout/*.hdf5')]}
 _TEST_FEAT_DROP = {'name': 'test_drop',
-        'data': [os.path.join(NEW_BASE_DIR, '*drop*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*drop*/*redyellow/*.hdf5')]}
 
 _TRAIN_LINKING = {'name': 'linking',
         'data': [os.path.join(BASE_DIR, 'linking/*/train/*.hdf5')]}
 _TRAIN_FEAT_LINKING = {'name': 'train_linking',
         'data': [os.path.join(BASE_DIR, 'linking/*/train_readout/*.hdf5')]}
 _TEST_FEAT_LINKING = {'name': 'test_linking',
-        'data': [os.path.join(NEW_BASE_DIR, '*linking*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*linking*/*redyellow/*.hdf5')]}
 
 _TRAIN_ROLLSLIDE = {'name': 'rollslide',
         'data': [os.path.join(BASE_DIR, 'rollingSliding/*/train/*.hdf5')]}
 _TRAIN_FEAT_ROLLSLIDE = {'name': 'train_rollslide',
         'data': [os.path.join(BASE_DIR, 'rollingSliding/*/train_readout/*.hdf5')]}
 _TEST_FEAT_ROLLSLIDE = {'name': 'test_rollslide',
-        'data': [os.path.join(NEW_BASE_DIR, '*rollingSliding*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*rollingSliding*/*redyellow/*.hdf5')]}
 
 _TRAIN_TOWERS = {'name': 'towers',
         'data': [os.path.join(BASE_DIR, 'towers/*/train/*.hdf5')]}
 _TRAIN_FEAT_TOWERS = {'name': 'train_towers',
         'data': [os.path.join(BASE_DIR, 'towers/*/train_readout/*.hdf5')]}
 _TEST_FEAT_TOWERS = {'name': 'test_towers',
-        'data': [os.path.join(NEW_BASE_DIR, '*towers*/*/*.hdf5')]}
+        'data': [os.path.join(NEW_BASE_DIR, '*towers*/*redyellow/*.hdf5')]}
 
 # Spaces
 SEEDS = list(range(_NUM_SEEDS))
@@ -112,7 +112,8 @@ METRICS_DATA = [
         ]
 
 TRAIN_DATA = TRAIN_DATA[:8] # single scenario
-# TRAIN_DATA = TRAIN_DATA[8:] # leave-one-out and all
+# TRAIN_DATA = TRAIN_DATA[8:] # leave-one-out
+# TRAIN_DATA = TRAIN_DATA[16:] #  all
 print(TRAIN_DATA)
 
 SPACE = construct_data_spaces(SEEDS, TRAIN_DATA, TRAIN_FEAT_DATA, TEST_FEAT_DATA, METRICS_DATA)
