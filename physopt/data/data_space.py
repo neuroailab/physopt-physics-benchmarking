7a9c8c0a2841660791242c6028b7c278a0809e92
Elias Wang <ewang921@gmail.com>
2021-09-01 16:11:30 -0700

    refactor get_data_space

--- physopt/data/data_space.py
+++ physopt/data/data_space.py
@@ -47,3 +47,30 @@ def construct_data_spaces(seeds, train_data, train_feat_data, test_feat_data, me
     spaces['test_feat'] = construct_extraction_space(seeds, train_data, test_feat_data)
     spaces['metrics'] = construct_metrics_space(seeds, train_data, metrics_data)
     return spaces
+
+def get_data_space(
+        data_space,
+        debug,
+        train_dir='/data1/eliwang/physion_train_data', # TODO
+        test_dir='/data1/eliwang/physion_test_data', # TODO
+        scenarios=['Collide', 'Contain', 'Dominoes', 'Drape', 'Drop', 'Link', 'Roll', 'Support'],
+        num_seeds=1,
+        ):
+
+    # Data subsets
+    train_data = []
+    train_feat_data = []
+    test_feat_data = []
+    for scenario in scenarios:
+        train_data.append({'name': scenario, 'data': [os.path.join(train_dir, 'dynamics_training', scenario, '*.hdf5')]}) # TODO: might need to make compatible with tfrecords too
+        train_feat_data.append({'name': scenario, 'data': [os.path.join(train_dir, 'readout_training', scenario, '*.hdf5')]})
+        test_feat_data.append({'name': scenario, 'data': [os.path.join(test_dir, 'model_testing', scenario, '*.hdf5')]})
+
+    # Spaces
+    seeds = list(range(num_seeds))
+
+    train_data = get_all_subsets(train_data) # TODO: have param specifying what train protocol all/abo/only
+    metrics_data = zip(train_feat_data, test_feat_data)
+
+    space = construct_data_spaces(seeds, train_data, train_feat_data, test_feat_data, metrics_data)
+    return space
