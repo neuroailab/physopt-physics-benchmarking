d5fda39a21b15f0bd750591cbdf8d808ed93bfe9
Eli Wang <ewang921@gmail.com>
2021-10-14 22:21:33 -0700

    move run_metrics to ReadoutObjective

--- physopt/utils/metrics.py
+++ physopt/utils/metrics.py
@@ -1,4 +1,8 @@
+import os
 import numpy as np
+import csv
+import logging
+import pickle
 
 class MetricModel(object):
     def __init__(self,
@@ -51,6 +55,118 @@ class MetricModel(object):
 def check_readout_model(readout_model):
     for attr in ['fit', 'predict', 'predict_proba']:
         assert hasattr(readout_model, attr) and callable(getattr(readout_model, attr)), f'Readout model must have {attr} method'
+
+def build_data(path, max_sequences = 1e9):
+    with open(path, 'rb') as f:
+        batched_data = pickle.load(f)
+
+    # Unpack batched data into sequencs
+    data = [] # each element in list is one example
+    for bd in batched_data:
+        for bidx in range(len(bd[list(bd.keys())[0]])):
+            sequence = {}
+            for k, v in bd.items():
+                sequence[k] = v[bidx]
+            data.append(sequence)
+            if len(data) > max_sequences:
+                break
+        if len(data) > max_sequences:
+            break
+
+    logging.info('Data len: {}'.format(len(data)))
+    logging.info('Input Shapes:')
+    for k, v in data[0].items():
+        try: 
+            logging.info('{} {}'.format(k, v.shape))
+        except Exception:
+            pass
+    return data
+
+def observed_model_fn(data):
+    states = np.concatenate([data['input_states'], data['observed_states']], axis=0)
+    states = np.reshape(states, [states.shape[0], -1])
+    return states
+
+def simulated_model_fn(data):
+    states = np.concatenate([data['input_states'], data['simulated_states']], axis=0)
+    states = np.reshape(states, [states.shape[0], -1])
+    return states
+
+def input_model_fn(data):
+    states = data['input_states']
+    states = np.reshape(states, [states.shape[0], -1])
+    return states
+
+def get_feature_fn(protocol):
+    if protocol == 'observed':
+        return observed_model_fn
+    elif protocol == 'simulated':
+        return simulated_model_fn
+    elif protocol == 'input':
+        return input_model_fn
+    else:
+        raise NotImplementedError('Unknown feature function!')
+
+def label_fn(data):
+    labels = data['labels'] # use full sequence for labels
+    labels = np.any(labels, axis=(0,1)).astype(np.int32).reshape([1])
+    return labels
+
+def get_num_samples(data, label_fn):
+    pos = []
+    neg = []
+    for i, d in enumerate(data):
+        if label_fn(d):
+            pos.append(i)
+        else:
+            neg.append(i)
+    return pos, neg
+
+def oversample(data, pos, neg):
+    balanced_data = []
+    if len(pos) < len(neg):
+        balanced_data = [data[i] for i in neg]
+        balanced_data.extend([data[i] for i in np.random.choice(pos, len(neg))])
+    elif len(neg) < len(pos):
+        balanced_data = [data[i] for i in pos]
+        balanced_data.extend([data[i] for i in np.random.choice(neg, len(pos))])
+    else:
+        balanced_data = data
+    return balanced_data
+
+def undersample(data, pos, neg):
+    balanced_data = []
+    if len(pos) < len(neg):
+        balanced_data = [data[i] for i in pos]
+        balanced_data.extend([data[i] for i in np.random.choice(neg, len(pos), replace=False)])
+    elif len(neg) < len(pos):
+        balanced_data = [data[i] for i in neg]
+        balanced_data.extend([data[i] for i in np.random.choice(pos, len(neg), replace=False)])
+    else:
+        balanced_data = data
+    return balanced_data
+
+def rebalance(data, label_fn, balancing = oversample):
+    # Get number of positive and negative samples
+    pos, neg = get_num_samples(data, label_fn)
+    logging.info("Before rebalancing: pos=%d, neg=%d" % (len(pos), len(neg)))
+
+    # Rebalance data by oversampling underrepresented calls
+    balanced_data = balancing(data, pos, neg)
+
+    pos, neg = get_num_samples(balanced_data, label_fn)
+    logging.info("After rebalancing: pos=%d, neg=%d" % (len(pos), len(neg)))
+    return balanced_data
+
+def write_metrics(results, metrics_file):
+    file_exists = os.path.isfile(metrics_file) # check before opening file
+    with open(metrics_file, 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames = list(results[0].keys()))
+        if not file_exists: # only write header once - if file doesn't exist yet
+            writer.writeheader()
+        writer.writerows(results)
+
+    logging.info('%d results written to %s' % (len(results), metrics_file))
 
 def squared_error(predictions, labels):
     assert predictions.ndim == 1
